fc73a4f22fbe322234a9bcea799e8b545de45c6a
obrague2002 <oliviarbrague@gmail.com>
2024-12-02 16:46:09 -0500

    Serial runtime

--- serialMain.c
+++ serialMain.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
+#include <time.h>
 #include "theDatabaser.h"
 #include "QueryParsing.h"
 #include "parse.h"
 
 
 int main(int argc, char** argv) {
+
+    clock_t start_time = clock();
     
     int numQueries = 0;
     //Turns Parameter into Parsed Array
@@ -12,6 +16,7 @@ int main(int argc, char** argv) {
 
     for(int i = 0; i < numQueries; i++)
     {
+        clock_t query_start_time = clock();
 
         printf("\nQuery %d\n\n", i);
 
@@ -33,5 +38,16 @@ int main(int argc, char** argv) {
 
         //Frees Memory
         freeDatabase(database);
+
+        clock_t query_end_time = clock();
+
+        double query_duration = (double)(query_end_time - query_start_time) / CLOCKS_PER_SEC;
+        printf("Query took %f seconds\n", query_duration);
     }
+
+    clock_t end_time = clock();
+    double total_duration = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("\nTotal runtime: %f seconds\n", total_duration);
+
+    return 0;
 }
