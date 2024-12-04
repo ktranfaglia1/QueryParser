8b3513e7f6acf023793b54066833e40ac2a04baf
Omniladder <dustintobrien@gmail.com>
2024-12-04 16:32:01 -0500

    Working 1 Query

--- QPEMPI.c
+++ QPEMPI.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <mpi.h>
 #include "theDatabaser.h"
 #include "QueryParsing.h"
 #include "parse.h"
@@ -13,44 +14,73 @@ int main(int argc, char** argv) {
     char*** parameters = getParams(argc, argv, &numQueries); 
     
     CarContainer** databases = (CarContainer**)malloc(sizeof(CarContainer*) * numQueries);
+    opTuple** postFix  = (opTuple**)malloc(sizeof(opTuple*) * numQueries);
+
+
+    //Creates a separate database and post fix for each query
+
+    
+    MPI_Init(&argc, &argv);
+
 
     for(int i = 0; i < numQueries; i++){
         databases[i] = createDatabase(parameters[i * 3 + 1][1]);
+
+         //Converts Operations to postFix
+        postFix[i] = where(parameters[i * 3 + 2]);
     }
-    
 
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get the rank of the process
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    for(int i = 0; i < numQueries; i++)
+    //int i = 0;
+    for(int i = rank; i < numQueries; i+= size)
     {
-        clock_t query_start_time = clock();
-
-        printf("\nQuery %d\n\n", i);
-
-        //Select Command Runs 
         
 
+        //printf("\nQuery %d\n\n", i);
+
+        CarContainer* database = (CarContainer*)malloc(sizeof(CarContainer));
+
+        database->size = databases[i]->size * numQueries / size;
+        database->array = (Car*)malloc(sizeof(Car) * database->size);
+
+        int counter = 0;
+        for(int j = database->size * rank; j < database->size * (rank + 1); j++){
+            database->array[counter] = databases[i]->array[j];
+            counter++;
+        }
+
         
-
-        //Converts Operations to postFix
-        opTuple* postFix = where(parameters[i * 3 + 2]);
-
         
-
         //Runs Operations Accordingly
-        CarContainer* database = callOperations(databases[i], postFix); 
+        database = callOperations(database, postFix[i]); 
+
+        
 
         //Outputs Database with proper columns
         printDatabase(database, parameters[i * 3]);
 
+        
+        
         //Frees Memory
         freeDatabase(database);
-
-        clock_t query_end_time = clock();
-
-        double query_duration = (double)(query_end_time - query_start_time) / CLOCKS_PER_SEC;
+        for(int i = 0; i < numQueries; i++){
+            freeDatabase(databases[i]);
+        }
+        
+        
 
     }
 
+    
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("End Program\n");
+
+    MPI_Finalize();
 
     return 0;
 }
