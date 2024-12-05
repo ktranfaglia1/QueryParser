ea118c82113cfa33ad338e410da49fc9f072bfa8
Omniladder <dustintobrien@gmail.com>
2024-12-04 19:12:57 -0500

    ''Working'' MPI

--- QPEMPI.c
+++ QPEMPI.c
@@ -4,6 +4,7 @@
 #include "theDatabaser.h"
 #include "QueryParsing.h"
 #include "parse.h"
+#include "unistd.h"
 
 
 int main(int argc, char** argv) {
@@ -30,29 +31,49 @@ int main(int argc, char** argv) {
         postFix[i] = where(parameters[i * 3 + 2]);
     }
 
+
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Get the rank of the process
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    //int i = 0;
-    for(int i = rank; i < numQueries; i+= size)
+    int i = rank % numQueries;
+    //for(int i = rank; i < numQueries; i+= size)
     {
+        //printf("Start\n");
+        int queryID[size];
+        
+       // printf("Ptr. %p, i: %p\n", queryID, &i);
+        MPI_Allgather(&i, 1, MPI_INT, queryID, 1, MPI_INT, MPI_COMM_WORLD);
+        
+        
+        int queryCount = 0;
+        //printf("Ptr. %p\n", queryID);
+
+        //printf("Checkpoint\n");
+        for(int j = 0; j < size; j++){
+            queryCount += (queryID[j] == i);
+        }
+        //printf("Checkpoint 2\n");
         
 
         //printf("\nQuery %d\n\n", i);
-
+        
         CarContainer* database = (CarContainer*)malloc(sizeof(CarContainer));
+        
+        
 
-        database->size = databases[i]->size * numQueries / size;
+        database->size = databases[i]->size / queryCount;
         database->array = (Car*)malloc(sizeof(Car) * database->size);
 
+        
+
         int counter = 0;
-        for(int j = database->size * rank; j < database->size * (rank + 1); j++){
+        for(int j = database->size * (rank % queryCount); j < database->size * (rank % queryCount + 1); j++){
             database->array[counter] = databases[i]->array[j];
             counter++;
         }
 
-        
+        //printf("Checkpoint\n");
         
         //Runs Operations Accordingly
         database = callOperations(database, postFix[i]); 
@@ -60,9 +81,17 @@ int main(int argc, char** argv) {
         
 
         //Outputs Database with proper columns
+        //printf("Thread %d\n", rank);
+        sleep(i * 1);
+        if(rank < numQueries){
+            printf("Query %d\n\n", i);
+        }
+        else{
+            sleep(.1);
+        }
         printDatabase(database, parameters[i * 3]);
 
-        
+        //2>/dev/null
         
         //Frees Memory
         freeDatabase(database);
@@ -70,15 +99,14 @@ int main(int argc, char** argv) {
             freeDatabase(databases[i]);
         }
         
-        
-
+        //printf("End\n");
     }
 
     
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    printf("End Program\n");
+    //printf("End Program\n");
 
     MPI_Finalize();
 
